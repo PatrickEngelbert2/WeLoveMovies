66fad8ffc9d6b539cbe84257c18c30abbfed760f
PatrickEngelbert2 <patrickengelbert2@gmail.com>
2021-12-22 13:10:49 -0600

    fixed failure when running npm run rollback

--- src/db/migrations/20211206104417_createReviewsTable.js
+++ src/db/migrations/20211206104417_createReviewsTable.js
@@ -21,5 +21,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-    return knex.schema.droptable("reviews")
+    return knex.schema.dropTable("reviews")
 };
--- src/db/migrations/20211206104520_createMovies_TheatersTable.js
+++ src/db/migrations/20211206104520_createMovies_TheatersTable.js
@@ -17,5 +17,5 @@ exports.up = function (knex) {
 };
 
 exports.down = function (knex) {
-  return knex.schema.droptable("movies_theaters");
+  return knex.schema.dropTable("movies_theaters");
 };
